5eac834ec8687bac11cbec08bdd1b434efbc29bf
Jan Pucilowski <jan.pucilowski99@gmail.com>
2024-02-13 16:33:41 +0100

    feat(kubernetes): add cross-seed and qbittorrent-tools configurations
     - Create new ExternalSecret and HelmRelease for cross-seed application
     - Add cross-seed Kustomization and HelmRelease to media applications
     - Introduce qbittorrent-tools HelmRelease and Kustomization
     - Include qbittorrent-tools configuration and resources

---
+++ kubernetes/apps/media/cross-seed/app/resources/config.js
@@ -0,0 +1,28 @@
+// Note: Cross-Seed vars should be escaped with $${VAR_NAME} to avoid interpolation by Flux
+module.exports = {
+  delay: 20,
+  qbittorrentUrl: "http://qbittorrent.default.svc.cluster.local",
+  torznab: [
+    `http://prowlarr.default.svc.cluster.local/9/api?apikey=$${process.env.PROWLARR_API_KEY}`, // 1337
+    `http://prowlarr.default.svc.cluster.local/10/api?apikey=$${process.env.PROWLARR_API_KEY}`, // fnp
+  ],
+  port: process.env.CROSSSEED_PORT || 80,
+  apiAuth: false,
+  action: "inject",
+  includeEpisodes: false,
+  includeSingleEpisodes: true,
+  includeNonVideos: true,
+  duplicateCategories: true,
+  matchMode: "safe",
+  skipRecheck: true,
+  linkType: "hardlink",
+  linkDir: "/media/Downloads/qbittorrent/complete/cross-seed",
+  dataDirs: [
+    "/media/Downloads/qbittorrent/complete/prowlarr",
+    "/media/Downloads/qbittorrent/complete/radarr",
+    "/media/Downloads/qbittorrent/complete/sonarr",
+  ],
+  maxDataDepth: 1,
+  outputDir: "/config/xseeds",
+  torrentDir: "/config/qBittorrent/BT_backup",
+};
